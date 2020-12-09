3c3f13a0ecd7b5bbb6dbc3089432b5ad9043bd42
Mitchel Noble <Mitch@Mitchels-MacBook-Pro.local>
2020-12-09 06:51:27 -0800

    continued AddPracticeRoutine

--- practice-practice-practice/src/components/AddPracticeRoutine.jsx
+++ practice-practice-practice/src/components/AddPracticeRoutine.jsx
@@ -46,6 +46,116 @@ const AddPracticeRoutine = (props) => {
 
   return (
     <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='topic'>So, what is your practice topic?:</label>
+        <input
+          type='text'
+          name='topic'
+          value={topic}
+          onChange={(e) => {
+            setTopic(e.target.value);
+          }}
+        />
+        <label htmlFor='specifics'>Get specific! What parts of your topic are you practicing?:</label>
+        <input
+          type='text'
+          name='specifics'
+          value={specifics}
+          onChange={(e) => {
+            setSpecifics(e.target.value);
+          }}
+          />
+        <label htmlFor='notes'>Any notes you'd like to write out?</label>
+        <input
+          type='text'
+          name='notes'
+          value={notes}
+          onChange={(e) => {
+            setNotes(e.target.value);
+          }}
+          />
+        <label htmlFor='daily'>Are you praciticing this topic everyday? (Yes/No): </label>
+        <input
+          type='text'
+          name='daily'
+          value={daily}
+          onChange={(e) => {
+            setDaily(e.target.value);
+          }}
+          />
+        <label htmlFor='frequency'>How many times a day?: </label>
+        <input
+          type='number'
+          name='frequency'
+          value={frequency}
+          onChange={(e) => {
+            setFrequency(e.target.value);
+          }}
+          />
+        <label htmlFor='hours'>How many hours a day?: </label>
+        <input
+          type='number'
+          name='hours'
+          value={hours}
+          onChange={(e) => {
+            setHours(e.target.value);
+          }}
+          />
+        <label htmlFor='monday'></label>
+        <input
+          type='text'
+          name=''
+          value=''
+          onChange={(e) => {
+            set(e.target.value);
+          }}
+          />
+        <label htmlFor=''></label>
+        <input
+          type=''
+          name=''
+          value=''
+          onChange={(e) => {
+            set(e.target.value);
+          }}
+          />
+        <label htmlFor=''></label>
+        <input
+          type=''
+          name=''
+          value=''
+          onChange={(e) => {
+            set(e.target.value);
+          }}
+          />
+        <label htmlFor=''></label>
+        <input
+          type=''
+          name=''
+          value=''
+          onChange={(e) => {
+            set(e.target.value);
+          }}
+          />
+        <label htmlFor=''></label>
+        <input
+          type=''
+          name=''
+          value=''
+          onChange={(e) => {
+            set(e.target.value);
+          }}
+          />
+        <label htmlFor=''></label>
+        <input
+          type=''
+          name=''
+          value=''
+          onChange={(e) => {
+            set(e.target.value);
+          }}
+          />
+      </form>
     </div>
   );
 };
