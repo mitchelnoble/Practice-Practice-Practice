992a3584c8d716c954f3ba9a82c55eb56d696c75
Mitchel Noble <Mitch@Mitchels-MacBook-Pro.local>
2020-12-11 01:29:55 -0800

    added emoji to nav bar

--- practice-practice-practice/src/components/Nav.jsx
+++ practice-practice-practice/src/components/Nav.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import "./Nav.css";
 
+let house = "🏡";
+let routine = "📝";
+
 const Nav = () => {
   return (
     <nav className="navigation">
-      <Link className="nav-link" to ='/'>| Home |</Link>
-      <Link className="nav-link" to ='/add-practice-routine'>| Setup Your Practice Routine |</Link>
+      <Link className="nav-link" to ='/'> Home {house} </Link>
+      <Link className="nav-link" to ='/add-practice-routine'> Setup Your Practice Routine {routine} </Link>
     </nav>
   );
 };
