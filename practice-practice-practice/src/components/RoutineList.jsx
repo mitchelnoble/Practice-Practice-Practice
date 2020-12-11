f8f802b9ee18f7253b722600845c43b820054ba3
Mitchel Noble <Mitch@Mitchels-MacBook-Pro.local>
2020-12-11 02:27:33 -0800

    adjusted color for nav bar, and styling for home page links

--- practice-practice-practice/src/components/RoutineList.jsx
+++ practice-practice-practice/src/components/RoutineList.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import PracticeRoutine from './PracticeRoutine';
-import Header from './Header';
-import RoutineList from './RoutineList';
-import "./Homepage.css"
+import PracticeRoutine from "./PracticeRoutine";
 
-const Homepage = ({ practiceRoutines }) => {
-  console.log(practiceRoutines)
+
+const RoutineList = ({practiceRoutines}) => {
   return (
-    <div>
-      <Header />
-      {practiceRoutines.map((practiceRoutine) => {
+    <div className="routine-list">
+            {practiceRoutines.map((practiceRoutine) => {
         return (
           <Link className="home-link" key={practiceRoutine.id} to={`/practiceRoutine/${practiceRoutine.id}`}>
             <div className="home-circle">
@@ -23,4 +19,4 @@ const Homepage = ({ practiceRoutines }) => {
   );
 };
 
-export default Homepage;
+export default RoutineList;
